111941b5be0e39112c882505affedf02aeb1bbf4
Scott Shidlovsky <shidlovsky.scott@gmail.com>
2017-11-22 15:57:33 -0500

    Break up security configuration to seperate file
    Having the security config in the main class was causing issues with the DataJpaTests plus it was just wrong.

--- server/src/main/java/com/maintenance/MaintenanceApplication.java
+++ server/src/main/java/com/maintenance/MaintenanceApplication.java
@@ -1,107 +1,11 @@
 package com.maintenance;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
-import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-
-import javax.servlet.Filter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
-@EnableOAuth2Client
-public class MaintenanceApplication extends WebSecurityConfigurerAdapter {
+public class MaintenanceApplication  {
 
-	@Autowired
-	OAuth2ClientContext oauth2ClientContext;
-
-	/**
-	 * Add authentication filters
-	 * Use cookie csrf protection to work with angular
-	 * Add custom entry point to always send 401 unauthorized
-	 */
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/**").authorizeRequests()
-				.antMatchers( "/login**").permitAll()
-				.anyRequest().authenticated()
-				.and().exceptionHandling()
-					.authenticationEntryPoint((HttpServletRequest request,
-													   HttpServletResponse response,
-													   AuthenticationException ex) -> {
-						response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-					})
-				.and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-				.and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
-	}
-
-	/**
-	 * Create a facebook oauth filter
-	 * @return
-	 */
-	private Filter ssoFilter() {
-		SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-		successHandler.setDefaultTargetUrl("/?login=success");
-		successHandler.setAlwaysUseDefaultTargetUrl(true);
-		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login");
-		facebookFilter.setAuthenticationSuccessHandler(successHandler);
-		OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
-		facebookFilter.setRestTemplate(facebookTemplate);
-		UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
-		tokenServices.setRestTemplate(facebookTemplate);
-		facebookFilter.setTokenServices(tokenServices);
-		return facebookFilter;
-	}
-
-	/**
-	 * Add oauth2 client context filter
-	 * @param filter
-	 * @return
-	 */
-	@Bean
-	public FilterRegistrationBean oauth2ClientFilterRegistration(
-			OAuth2ClientContextFilter filter) {
-		FilterRegistrationBean registration = new FilterRegistrationBean();
-		registration.setFilter(filter);
-		registration.setOrder(-100);
-		return registration;
-	}
-
-
-	/**
-	 * Get facebook authorization client configuration
-	 */
-	@Bean
-	@ConfigurationProperties("facebook.client")
-	public AuthorizationCodeResourceDetails facebook() {
-		return new AuthorizationCodeResourceDetails();
-	}
-
-	/**
-	 * Get facebook authorization resource configuration
-	 */
-	@Bean
-	@ConfigurationProperties("facebook.resource")
-	public ResourceServerProperties facebookResource() {
-		return new ResourceServerProperties();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaintenanceApplication.class, args);
